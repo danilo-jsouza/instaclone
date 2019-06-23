5fdf522ae4c2ebfd25f9d4d07f40cd87244b1d0e
danilo-jsouza <danilo.jsouza02@gmail.com>
2019-06-23 02:55:24 -0300

    instaclone

---
+++ frontend/frontend/src/components/Header/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import camera from '../../assets/camera.svg';
+
+export default function Header() {
+    return(
+        <header id="main-header">
+            <div className="header-content">
+                <Link to="/">
+                    <img src={logo} alt="InstaClone" />
+                </Link>
+                <Link to="/new">
+                    <img src={camera} alt="Enviar Publicação" />
+                </Link>
+            </div>
+        </header>
+    )
+}
